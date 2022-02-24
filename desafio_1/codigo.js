a8564994c41f78f30314f2828cf37a6a8bcd91eb
Fernando <fer0osennin@gmail.com>
2022-02-23 18:25:11 -0600

    cambios menores

--- desafio_1/codigo.js
+++ desafio_1/codigo.js
@@ -1,51 +1,56 @@
-//inicio
-alert("Bienvenido, esta es una caluladora de Indice de Masa Corporal")
-//introduce los datos
-const sexo = prompt("para iniciar Escriba su sexo M para pasculino F para femenino ")
-let edad = prompt("Digite su edad")
-let altura = prompt ("digite su altura, ejemplo 1m 80 cm es igual a 1.80cm")
-let peso = prompt("Digite su peso en kg")
+// inicio
+alert('Bienvenido, esta es una caluladora de Indice de Masa Corporal')
+// introduce los datos
+const sexo = prompt(
+  'para iniciar Escriba su sexo M para pasculino F para femenino '
+)
+// let edad = prompt('Digite su edad')
+let altura = prompt('digite su altura, ejemplo 1m 80 cm es igual a 1.80cm')
+let peso = prompt('Digite su peso en kg')
 // conversion de datos
-altura = parseFloat(altura) 
+altura = parseFloat(altura)
 peso = parseInt(peso)
-edad = parseInt(edad)
+// edad = parseInt(edad)
 // manejo de excepiones
-try{
-    if(altura === ''){
-        alert("no has escrito nada en altura")
-    }
-    else if (peso === ''){
-        alert("No has escrito nada en peso")
-    }
-    else if (sexo === ''){
-        alert("No has escrito tu sexo")
-    }
-}catch{
-    console.error("datos no validos o faltantes")
+try {
+  if (altura === '') {
+    alert('no has escrito nada en altura')
+  } else if (peso === '') {
+    alert('No has escrito nada en peso')
+  } else if (sexo === '') {
+    alert('No has escrito tu sexo')
+  }
+} catch {
+  console.error('datos no validos o faltantes')
 }
 // calculo con formula peso (kg) / estatura **2
-let calculo = (peso /(altura**2))
+let calculo = peso / altura ** 2
 
-switch (sexo){
-    case 'F':
-         if(edad >20){
-            if(calculo <20){
-                alert("Usted tiene un peso inferior al normal")
-            }
-         else if(calculo >=20 && calculo <24){
-                alert("Peso normal")
-            }
-        else if (calculo >=24 && calculo <29){
-                alert("peso superior a lo nromal")
-            }
-        else{
-                alert("Tienes sobrepeso")
-            }
-        }
-        break
-    case 'M':
-        if(calculo <21){
-            alert()
-        }
-
+switch (sexo) {
+  case 'F':
+    if (calculo < 20) {
+      alert('Indice de Masa Muscular bajo')
+    } else if (calculo >= 20 && calculo <= 23.9) {
+      alert('Indice de Masa Muscular Normal')
+    } else if (calculo >= 24 && calculo <= 28.9) {
+      alert('tienes obesidad leve')
+    } else if (calculo >= 29 && calculo <= 37) {
+      alert('Tienes obesidad severa')
+    } else if (calculo > 37) {
+      alert('Tienes obesidad severa')
+    }
+    break
+  case 'M':
+    if (calculo < 20) {
+      alert('Indice de Masa Muscular bajo')
+    } else if (calculo >= 20 && calculo <= 24.9) {
+      alert('Indice de Masa Muscular Normal')
+    } else if (calculo >= 25 && calculo <= 29.9) {
+      alert('tienes obesidad leve')
+    } else if (calculo >= 30 && calculo <= 40) {
+      alert('Tienes obesidad severa')
+    } else if (calculo > 40) {
+      alert('Tienes obesidad severa')
+    }
+    break
 }
