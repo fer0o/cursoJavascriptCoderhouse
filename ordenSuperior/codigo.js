88ade22010c9ecff06a4e5f752a1397a98ab7d7f
fer0o <fer0osennin@gmail.com>
2022-03-02 18:38:47 -0600

    practica de funciones

---
+++ ordenSuperior/codigo.js
@@ -0,0 +1,43 @@
+function asignarOperacion (op) {
+  //funcion qasignar Opracion que recibe como parametro una funcion llamda op
+  if (op === 'sumar') {
+    //si op es igual a sumar
+    return (a, b) => a + b //regresa una funcion que recibe como parametros a, b y regresa la suma de a + b
+  } else if (op === 'restar') {
+    // si no, si op es igual a restar regresa una funcion que recibe como parametros a,b y regresa la resta de a - b
+    return (a, b) => a - b
+  }
+}
+
+let suma = asignarOperacion('sumar') // definimos la cariable suma con funcion asignarOperacion, funcion anterior que debia recibir 'suma o resta'
+let resta = asignarOperacion('restar') // definimos la cariable suma con funcion asignarOperacion, funcion anterior que debia recibir 'suma o resta'
+
+//console.log(suma(4, 6)) //imprimimos en consola la variable suma y le damos como parametros (4, 6) debido a que suma recibe 2 parametros numericos
+//console.log(resta(10, 3)) //imprimimos en consola la variable resta y le damos como parametros (6, 7) debido a que resta recibe 2 parametros numericos
+
+//  Recibir funciones por parametro
+// Esta funcion recibe un array por primer parametro y una funcion por el segundo
+// Recorre el array y por cada elemento hace un llamado a la funcion mencionada
+// enviando dicho elemento por parametro
+
+function porCadaUNo (arr, fn) {
+  for (const el of arr) {
+    fn(el)
+  }
+}
+const numeros = [1, 2, 3, 4, 5]
+//porCadaUNo(numeros, console.log)
+
+let total = 0
+function acumular (num) {
+  total += num
+}
+porCadaUNo(numeros, acumular)
+//console.log(total)
+
+function regresaFuncion () {
+  return () => {
+    console.log('Hola Mundo')
+  }
+}
+regresaFuncion()()
